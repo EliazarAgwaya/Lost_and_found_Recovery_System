97a2160d7593ba7622705e9f116b1d39072eeebc
KN-cod <ezra.njeru@student.moringaschool.com>
2022-05-19 12:25:03 +0300

    validate login

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,13 +2,23 @@ let loginBtn = document.getElementById('login')
 let emailEl =document.getElementById('email')
 let passEl = document.getElementById('pswd')
 let errorTxt = document.getElementById('error-txt')
+let passError = document.getElementById('pass-error')
+let mailError = document.getElementById('mail-error')
 
 loginBtn.addEventListener('click', (e) =>{
     e.preventDefault()
-    if(emailEl.value == '' && passEl == ""){
+    if(emailEl.value == '' || passEl.value == ''){
         errorTxt.style.display = 'block'
     }else{
-        window.location.href = "../../index.html"    
+        let emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        if(emailEl.value.match(emailFormat)){
+            let password = passEl.value
+            if(password.length < 8 || password.length > 15){
+                passError.style.display = 'block'
+            }else{
+                window.location.href = "../../index.html"
+            }}else{
+                mailError.style.display = 'block'
+            }
     }
-
 })
